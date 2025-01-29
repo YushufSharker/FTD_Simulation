781b6b0228485be6ec1615292efa14f8593a99c7
YushufSharker <yushuf.sharker@takeda.com>
2025-01-29 16:12:42 -0500

    Updated and consistency checked, error message, singularity and warning data collected

--- model_fit.R
+++ model_fit.R
@@ -4,8 +4,15 @@ mmrm_fit <- function(data = dat3) {
   fit <- mmrm::mmrm(
     formula = chg ~ group * Mcat + ar1(Mcat | id),
     data = data,
-    control = mmrm_control(method = "Kenward-Roger")
+    control = mmrm_control(method = "Kenward-Roger", accept_singular = TRUE)
   )
+  # Calculate the condition number
+  condition_number <- kappa(model.matrix(fit))
+
+  sing <- ifelse(condition_number > 1e+04, "Singular", "Non-Singular")
+  wtext <- names(warnings())
+  w1 = get_phrase(wtext, "unidentifiable")
+  w2 = get_phrase(wtext, "failed to converge")
 
   prop_change_mmrm <- tibble(as.data.frame(emmeans(fit, ~ group |
                             Mcat, type = "response")[9:10])) %>%
@@ -15,7 +22,7 @@ mmrm_fit <- function(data = dat3) {
   model = 'MMRM'
   dgm = unique(data["dgm"])
   errm = unique(data["errm"])
-  return(tibble(prop_change_mmrm, estimate, pvalue, model, dgm, errm))
+  return(tibble(dgm, errm, prop_change_mmrm, estimate, pvalue, model, sing, w1, w2))
 }
 
 #Ref resource for MMRM https://cran.r-project.org/web/packages/mmrm/vignettes/introduction.html
