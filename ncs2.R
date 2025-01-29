19f080301184b10227851d6665f71910bc04fdca
YushufSharker <yushuf.sharker@takeda.com>
2025-01-29 16:09:37 -0500

    Upgrades to make it consistent. Also, captures some additionaal information from data

--- ncs2.R
+++ ncs2.R
@@ -15,12 +15,16 @@ Func_ncs_int <- function(data = dat1, last_visit = 24) {
   }
 
     fit_ncs_int <- lmer(chg ~
-                        I(ns21(M)) +
-                        I(ns22(M)) +
-                        (I(ns21(M))):group +
-                        I(ns22(M)):group +
-                        (1 | id), data = data)
-
+                        I(ns21(month)) +
+                        I(ns22(month)) +
+                        (I(ns21(month)) +
+                         I(ns22(month))):group +
+                        (1| id),
+                        data = data,
+                        control = lmerControl(check.nobs.vs.nRE = "ignore")
+                        )
+  sing <- ifelse(isSingular(fit_ncs_int) == TRUE, "Singular", "Non-Singular")
+  wtext <- names(warnings())
   # Extract the fitted values and residuals
   dds <- data %>%
     mutate(fitted_values = fitted(fit_ncs_int),
@@ -36,35 +40,53 @@ Func_ncs_int <- function(data = dat1, last_visit = 24) {
   # Compute the mean square error (MSE) for M54
   MSE <- mean(dd1_M24$squared_residuals)
 
+  # out_ncs_ranslp <- ref_grid(fit_ncs_int,
+  #                            at = list(M = 24,group = as.factor(0:1)),
+  #                            data = data,
+  #                            mode = "kenward-roger") %>%
+  #   emmeans(specs = "group",
+  #           month=24,
+  #           lmerTest.limit=2000) %>%
+  #   pairs(reverse=TRUE) %>%
+  #   as.data.frame() %>%
+  #   mutate(mod = "ncs-uns")
+
+
   out_ncs_ranslp <- ref_grid(
     fit_ncs_int,
     at = list(M = 24, group = c("0", "1")),
     data = data,
-    mode = "satterthwaite",
-    lmerTest.limit = 15000
+    mode = "kenward-roger"    , lmerTest.limit = 15000
   ) %>%
     emmeans(specs = 'group',
-            by = 'M',
-            lmerTest.limit = 15000) #%>%
+            month=24,
+            lmerTest.limit = 2000) #%>%
+
   prop_change_cs2 <- out_ncs_ranslp %>% as.data.frame() %>% tibble() %>%
-    with(., round(1 - .[2, 3] / .[1, 3], 2)) #%>% dplyr::rename(c("Prop"="emmean"))
+    with(., round(1 - .[2, 2] / .[1, 2], 2)) %>% dplyr::rename(c("Prop"="emmean"))
 
   dgm = unique(data["dgm"])
   errm = unique(data["errm"])
+  w1 = get_phrase(wtext, "unidentifiable")
+  w2 = get_phrase(wtext, "failed to converge")
 
   output <- bind_cols(
-    prop_change_cs2,
+    dgm, errm,prop_change_cs2,
     out_ncs_ranslp %>% pairs(reverse = TRUE) %>% as.data.frame() %>%
       mutate(
         estimate = round(estimate, 2),
-        #p.value_ratio = round(p.value, 5),
+        p.value_ratio = round(p.value, 5),
         mod = 'ncs-ranslp',
         #MSE=MSE,
         # SE.score = SE,
         # df.score = df,
         #t.ratio.score = t.ratio,
-        p.value.score = round(p.value, 5)
-      )%>% select(estimate, pvalue = p.value.score, model = mod), dgm, errm
+        #p.value.score = round(p.value, 5),
+        singularity = sing,
+        war1 = w1,
+        war2 = w2
+      )%>% select(estimate, pvalue = p.value_ratio, model = mod, singularity,
+                  war1, war2)
 
   )
   return(output)
